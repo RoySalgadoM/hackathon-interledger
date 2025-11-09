03e8bd521799ffab63073ec072cf836928614a3f
dev-mrpr <mauricio.perez@exos.mx>
2025-11-09 01:35:35 -0600

    [front] load wallets in on mounted

--- hackathon-interledger-front/src/App.vue
+++ hackathon-interledger-front/src/App.vue
@@ -2,4 +2,17 @@
   <router-view />
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+import { useWalletStore } from '@/stores/wallet'
+
+const walletStore = useWalletStore()
+
+onMounted(async () => {
+  try {
+    await walletStore.getWallets()
+  } catch (error) {
+    console.error('Error fetching wallets:', error)
+  }
+})
+</script>
